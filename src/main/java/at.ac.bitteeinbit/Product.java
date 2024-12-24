06f89504c849a3ba17ae714ed572be9151bb43c6
Gabriel Grabka <ggrabka@hotmail.com>
2024-12-24 16:26:02 +0100

    Added gson library, Product class and products.json

---
+++ src/main/java/at.ac.bitteeinbit/Product.java
@@ -0,0 +1,24 @@
+package at.ac.bitteeinbit;
+
+public class Product {
+    private int productId,price;
+    private String productGroup,name;
+
+    public Product(int price, String productGroup, String name) {
+        this.price = price;
+        this.productGroup = productGroup;
+        this.name = name;
+    }
+
+    public void getProduct() {
+        //toDo
+    }
+
+    public void addProduct() {
+        //toDo
+    }
+
+    public void removeProduct() {
+        //toDo
+    }
+}
