c0c59b572aaf97d7102766e1ae113ffa316f26ea
gab <gab@DESKTOP-FA09AIL>
2024-12-28 01:15:04 +0100

    Added FileReader

--- src/main/java/at.ac.bitteeinbit/Main.java
+++ src/main/java/at.ac.bitteeinbit/Main.java
@@ -4,5 +4,16 @@ package at.ac.bitteeinbit;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World");
+
+        Product test = new Product(10,"Gebäck","Pizza");
+        test.testJson();
+
+
+
+
     }
+
+
+
+
 }
--- src/main/java/at.ac.bitteeinbit/Product.java
+++ src/main/java/at.ac.bitteeinbit/Product.java
@@ -2,6 +2,9 @@ package at.ac.bitteeinbit;
 
 import com.google.gson.Gson;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class Product {
     private int productId,price;
     private String productGroup,name;
@@ -16,9 +19,24 @@ public class Product {
         return this;
     }
 
-    public void addProduct(int price, String productGroup, String name) {
-        //check if product is already in product json
+    public void testJson() {
         Gson gson = new Gson();
+
+        try {
+            FileReader reader = new FileReader("products.json");
+            Product test = gson.fromJson(reader,Product.class);
+            System.out.println(test.name);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public void addProduct(int price, String productGroup, String name) throws FileNotFoundException {
+        //check if product is already in product json
+
+
+
     }
 
     public void removeProduct() {
