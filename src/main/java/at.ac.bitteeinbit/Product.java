97e63f6b71719d9eb57e59142a38a0acb05a5e16
Gabriel Grabka <ggrabka@hotmail.com>
2024-12-28 12:08:00 +0100

    Created first draft of adding Product

--- src/main/java/at.ac.bitteeinbit/Product.java
+++ src/main/java/at.ac.bitteeinbit/Product.java
@@ -1,15 +1,22 @@
 package at.ac.bitteeinbit;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Product {
-    private int productId,price;
-    private String productGroup,name;
+    private int productId;
+    private double price;
+    private String productGroup;
+    private String name;
+    List<Product> products;
 
-    public Product(int price, String productGroup, String name) {
+    public Product(double price, String productGroup, String name) {
         this.price = price;
         this.productGroup = productGroup;
         this.name = name;
@@ -19,27 +26,48 @@ public class Product {
         return this;
     }
 
-    public void testJson() {
-        Gson gson = new Gson();
+    public double getPrice() {
+        return price;
+    }
 
-        try {
-            FileReader reader = new FileReader("products.json");
-            Product test = gson.fromJson(reader,Product.class);
-            System.out.println(test.name);
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
+    public String getProductGroup() {
+        return productGroup;
+    }
 
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public void setProductGroup(String productGroup) {
+        this.productGroup = productGroup;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public void addProduct(int price, String productGroup, String name) throws FileNotFoundException {
         //check if product is already in product json
-
-
-
     }
 
     public void removeProduct() {
         //toDo
+    }
+
+    @Override
+    public String toString() {
+        return "{productId=" + productId + "productGroup=" + productGroup + ", name=" + name + ", price= " + price + "}";
     }
 }
